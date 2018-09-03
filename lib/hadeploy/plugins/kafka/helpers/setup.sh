53d2beda91d466ce1460e38785f9d74a5c68af51
Serge ALEXANDRE <sa@broadsoftware.com>
2018-09-03 22:31:22 +0200

    Adapt kafka setup.sh.

--- lib/hadeploy/plugins/kafka/helpers/setup.sh
+++ lib/hadeploy/plugins/kafka/helpers/setup.sh
@@ -21,15 +21,19 @@ MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 JDCTOPIC_VERSION="0.2.1"
 
-if [ ! -f $MYDIR/jdctopic/jdctopic_uber-${JDCTOPIC_VERSION}.jar ]
-then
-	echo 
-	echo "******************** Will download jdctopic_uber-${JDCTOPIC_VERSION}.jar"
-	echo 
-	curl -L https://github.com/Kappaware/jdctopic/releases/download/v${JDCTOPIC_VERSION}/jdctopic_uber-${JDCTOPIC_VERSION}.jar -o $MYDIR/jdctopic/jdctopic_uber-${JDCTOPIC_VERSION}.jar
-else
-	echo "-------------------- jdctopic_uber-${JDCTOPIC_VERSION}.jar already downloaded"
-fi
+for KAFKA_VERSION in "0.10.0" "1.0.0" "1.1.1" "2.0.0"
+do
+	JAR="jdctopic.${KAFKA_VERSION}-${JDCTOPIC_VERSION}-uber.jar"
+	if [ ! -f ${MYDIR}/jdctopic/${JAR} ]
+	then
+		echo 
+		echo "******************** Will download jdctopic_uber-${JDCTOPIC_VERSION}.jar"
+		echo 
+		curl -L https://github.com/Kappaware/jdctopic/releases/download/v${JDCTOPIC_VERSION}/${JAR} -o $MYDIR/jdctopic/${JAR}
+	else
+		echo "-------------------- ${JAR} already downloaded"
+	fi
+done
 
 
 
